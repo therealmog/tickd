961f1b1c6e2b2213e6e98dae7ebd4517d5638476
therealmog <amoghg75@yahoo.com>
2024-08-05 08:00:24 +0100

    Implementing new submit button

--- today.py
+++ today.py
@@ -1,6 +1,7 @@
 from customtkinter import *
 from datetime import date
 from getDetails import getDetails,getDetailsIndividual
+from submitBtn import SubmitButton
 
 from PIL import Image
 
@@ -15,14 +16,17 @@ class Today(CTk):
         self.title("Today - Tickd")
 
         self.userDetails = getDetailsIndividual(email)
-
-        self.userName = self.userDetails[2]
+        try:
+            self.userName = self.userDetails[2]
+        except TypeError:
+            print("Invalid email entered. Have you registered?")
 
         self.today = date.today()
         self.todaysDate = self.today.strftime("%A, %d %B %Y")
         print(self.todaysDate)
 
         set_appearance_mode("Dark")
+        self.accent = "dodgerblue2"
         #self.bind("<Configure>",lambda event:self.mode())
         
 
@@ -50,10 +54,13 @@ class Today(CTk):
         
         self.textVar = f"Welcome, {self.userName}!"
         self.lblWelcome = CTkLabel(self.frameToday,text=self.textVar,font=(globalFontName,20))
-        self.logo = CTkImage(light_image=Image.open("logo//whiteBGLogo.png"),dark_image=Image.open("logo//blackBGLogo.png"),size=(165,53)) 
-        self.logoPanel = CTkLabel(self.frameToday,text="",image=self.logo)
-        self.taskEntry = CTkEntry(self.frameToday,placeholder_text="Enter a task...",font=(globalFontName,30),width=650,corner_radius=20)
+        self.imgLogo = CTkImage(light_image=Image.open("logo//whiteBGLogo.png"),dark_image=Image.open("logo//blackBGLogo.png"),size=(165,53)) 
+        self.logoPanel = CTkLabel(self.frameToday,text="",image=self.imgLogo)
+        self.entryTask = CTkEntry(self.frameToday,placeholder_text="Enter a task...",font=(globalFontName,30),width=650,corner_radius=20)
         
+        self.lblEnterUsername = CTkLabel(self.frameToday,text="Please enter your username:",font=(globalFontName,22))
+        self.entryUserName = CTkEntry(self.frameToday,placeholder_text="username",font=(globalFontName,22),width=200)
+        self.btnSubmitUsername = SubmitButton(parent=self.frameToday,command=self.submitUsername,colour=self.accent,xSize=40,ySize=40)
     
     def placeWidgets(self):
         self.frameToday.place(relx=0.5,rely=0.5,anchor="center")
@@ -62,12 +69,18 @@ class Today(CTk):
         self.lblDate.place(x=25,y=25)
         self.lblWelcome.place(in_=self.lblDate,x=0,y=40)
         self.logoPanel.place(relx=0.87,y=20)
-        self.taskEntry.place(in_=self.lblDate,x=400,y=10)
+        self.entryTask.place(in_=self.lblDate,x=400,y=10)
+        
         
     
     def checkUserName(self):
         if self.userName == "":
-            pass
+            print("You haven't got a username!")
+            self.lblEnterUsername.place(in_=self.lblWelcome,y=60)
+            self.entryUserName.place(in_=self.lblEnterUsername,x=280)
+            self.btnSubmitUsername.place(in_=self.entryUserName,x=150)
 
+    def submitUsername(self):
+        pass
 
-today = Today(email="amoghng")
+today = Today(email="omar@gmail.com")
