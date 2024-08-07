00ddf0fb0bb7b606fa3ca41b6cc9b2fb9db116a6
therealmog <amoghg75@yahoo.com>
2024-08-07 23:53:08 +0100

    nuttin really

---
+++ test.py
@@ -0,0 +1,14 @@
+elements = {
+            "lblEmail":1,
+            "lblPassword":2,
+            "entryEmail":3,
+            "entryPassword":4,
+            "btnRegister":5,
+            "btnSignIn":6,
+            "btnShowPassword":7,
+            "btnHidePassword":8,
+            "btnConfirm":9
+            }
+
+for each in elements:
+    print(each)
