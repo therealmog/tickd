a2566f57b857a0cbf93d0da202cd9e9c090fc9e5
therealmog <amoghg75@yahoo.com>
2024-08-19 11:29:05 +0100

    Worked on framework for adding new tasks

--- lib/createTaskDict.py
+++ lib/createTaskDict.py
@@ -1,2 +1,20 @@
+from random import choice
+from string import ascii_letters,ascii_lowercase,ascii_uppercase
 
-def createTaskDict(title,date,attributes={})
+def createTaskDict(title,date,attributes:dict=None):
+    chars = ascii_letters+ascii_uppercase+ascii_lowercase
+    taskID = ""
+    for each in range(6):
+        char = choice(chars)
+        taskID+=char
+
+    taskDict = {"title":title,
+                "date":date,
+                "taskID":taskID}
+
+    if attributes is not None:
+        for each in attributes:
+            item = attributes[each]
+            taskDict[each] = item
+    
+    return taskDict
